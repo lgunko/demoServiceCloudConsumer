52911ad89d93f53ea4f18435bdbc4cfd55fde3a4
D067044 <leonid.gunko@sap.com>
2019-12-03 15:27:40 +0100

    auth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import { ThemeProvider } from '@ui5/webcomponents-react/lib/ThemeProvider';
-import { Bar } from './components/bar.component'
-import { NavBar } from './components/navbar.component'
-import { MyTable } from './components/table.component'
+
+import { MainComponent } from './components/main.component'
 
 
 function App() {
   return (
     <div className="App" style={{ height: "100%" }}>
-      <ThemeProvider withToastContainer>
-        <div style={{ height: "100%" }}>
-          <Bar />
-          <div style={{ display: "flex", height: "calc(100% - 40px)" }}>
-            <NavBar />
-            <MyTable />
-          </div>
-        </div>
-      </ThemeProvider>
+      <MainComponent />
     </div>
   );
 }
---
+++ src/components/main.component.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { ThemeProvider } from '@ui5/webcomponents-react/lib/ThemeProvider';
+import { Bar } from './bar.component'
+import { NavBar } from './navbar.component'
+import { MyTable } from './table.component'
+const queryString = require('query-string');
+
+
+const LoginURL = "https://c4id-iam-test-one.accounts400.ondemand.com/oauth2/authorize";
+
+
+const RedirectUrl = "https://aa4tm323i6.execute-api.eu-central-1.amazonaws.com/Prod/callbackGetTokenByCode";
+const IasClientId = "T000003";
+const OIDCScope = "openid";
+const CodeResponseType = "code";
+const State = "longstatestate";
+
+
+export class MainComponent extends React.Component {
+
+    componentDidMount() {
+        console.log(window.sessionStorage.getItem("id_token"))
+        if (!window.sessionStorage.getItem("id_token")) {
+            if (queryString.parse(window.location.search).id_token) {
+                window.sessionStorage.setItem("id_token", queryString.parse(this.props.location.search).id_token)
+                window.location = window.location.origin
+            } else {
+                const redirectToUrl = new URL(LoginURL);
+                redirectToUrl.searchParams.append('redirect_uri', RedirectUrl);
+                redirectToUrl.searchParams.append('client_id', IasClientId);
+                redirectToUrl.searchParams.append('scope', OIDCScope);
+                redirectToUrl.searchParams.append('response_type', CodeResponseType);
+                redirectToUrl.searchParams.append('state', State);
+                window.location = redirectToUrl
+            }
+        }
+    }
+
+    render() {
+        return (
+            window.sessionStorage.getItem("id_token") &&
+            <ThemeProvider withToastContainer>
+                <div style={{ height: "100%" }}>
+                    <Bar />
+                    <div style={{ display: "flex", height: "calc(100% - 40px)" }}>
+                        <NavBar />
+                        <MyTable />
+                    </div>
+                </div>
+            </ThemeProvider>
+        )
+    }
+
+}

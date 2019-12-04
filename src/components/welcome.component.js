6722aa07a22dfd29b1e2d6d93facbd4c17c383a1
D067044 <leonid.gunko@sap.com>
2019-12-04 13:37:13 +0100

    1

--- src/components/welcome.component.js
+++ src/components/welcome.component.js
@@ -7,6 +7,16 @@ import { TableCell } from '@ui5/webcomponents-react/lib/TableCell';
 import { Text } from '@ui5/webcomponents-react/lib/Text';
 import { Card } from '@ui5/webcomponents-react/lib/Card'
 
+function parseJwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
+
 export class Welcome extends React.Component {
     render() {
         let name = parseJwt(window.sessionStorage.getItem("id_token"))["first_name"]

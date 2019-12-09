ca8bc5516939b0940ae053f12add33d3fc7ecd20
D067044 <leonid.gunko@sap.com>
2019-12-09 15:04:22 +0100

    1

--- src/components/clients.component.js
+++ src/components/clients.component.js
@@ -115,7 +115,7 @@ export class Clients extends React.Component {
                                 {
                                     rowData: [
                                         'SAP Service Cloud',
-                                        'http://bundle-701714770.eu-central-1.elb.amazonaws.com/bundle/federator/v1/location/bundle.tar.gz',
+                                        <a href='http://bundle-701714770.eu-central-1.elb.amazonaws.com/bundle/federator/v1/location/bundle.tar.gz'>http://bundle-701714770.eu-central-1.elb.amazonaws.com/bundle/federator/v1/location/bundle.tar.gz</a>,
                                         <span style={{ color: "green" }}>Active</span>,
                                         <span>{this.state && (this.state.sscLoading ? "Updating now..." : (this.state.sscFetchedLast + ' minutes ago'))}</span>,
                                         <Button glyph="edit" option="light" />,
@@ -125,7 +125,7 @@ export class Clients extends React.Component {
                                 {
                                     rowData: [
                                         'SAP Customer Data Platform',
-                                        'http://bundle-701714770.eu-central-1.elb.amazonaws.com/bundle/federator/v1/location/bundle.tar.gz',
+                                        <a href='http://bundle-701714770.eu-central-1.elb.amazonaws.com/bundle/federator/v1/location/bundle.tar.gz'>http://bundle-701714770.eu-central-1.elb.amazonaws.com/bundle/federator/v1/location/bundle.tar.gz</a>,
                                         <span style={{ color: "green" }}>Active</span>,
                                         <span>{this.state && (this.state.ssmLoading ? "Updating now..." : (this.state.ssmFetchedLast + ' minutes ago'))}</span>,
                                         <Button glyph="edit" option="light" />,
